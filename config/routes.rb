db993ef66ced3ad0be1a420ca9f6403a522e624c
Emerson Freitas <mersonsi@gmail.com>
2021-10-06 21:25:58 -0300

    Create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :applicants, only: [:index, :new, :create]
+  resources :positions
+  resources :companies, only: [:new, :edit, :update, :create]
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
