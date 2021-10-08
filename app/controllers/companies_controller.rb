3b592d7198369b8d4e6cedf1f96024cfc8c5d63e
Emerson Freitas <mersonsi@gmail.com>
2021-10-07 22:20:39 -0300

    refactor(Company): form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,25 +4,33 @@ class CompaniesController < ApplicationController
   end
 
   def edit
+    @company = current_user.company
   end
 
   def create
     @company = current_user.build_company(company_params)
     if @company.save
-      flash[:notice] = "Empresa cadastrada com sucesso."
+      flash[:notice] = 'Empresa cadastrada com sucesso.'
       redirect_to root_path
     else
-      flash[:error] = "Erro ao cadastrar a empresa."
+      flash[:error] = 'Erro ao cadastrar a empresa.'
       render :new
     end
   end
 
   def update
+    @company = current_user.company
+    if @company.update(company_params)
+      flash[:notice] = 'Empresa atualizada com sucesso.'
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def company_params
-    params.require(:company).permit(:name, :url)
+    params.require(:company).permit(:name, :url, :logo)
   end
 end
